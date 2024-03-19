39487ec0c3fa306286e775d1ec6b0c4c06b2580c
ruvick <rudikov.web@ya.ru>
2024-03-19 17:56:53 +0300

    Preloader

---
+++ src/js/files/preloader.js
@@ -0,0 +1,8 @@
+// Preloader
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
